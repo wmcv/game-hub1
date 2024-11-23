0ae2f7105bbce6d331ed9aa0dcda90b1bb1d46dc
William McVicar <wmcvicar@uwaterloo.ca>
2024-11-23 18:03:05 -0500

    Filter By Genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
 import useData from "./useData";
+import { Genre } from "./useGenres";
 
 
 export interface Platform {
@@ -18,6 +19,6 @@ export interface Game {
   
   
 
-const useGames = () => useData<Game>("/games");
+const useGames = (selectedGenre : Genre | null) => useData<Game>("/games", { params: { genres: selectedGenre?.id}}, [selectedGenre?.id]);
 
 export default useGames;
