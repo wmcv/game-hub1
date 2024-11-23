494837aa318eecd0b043e11f0bb805630d706f3d
William McVicar <wmcvicar@uwaterloo.ca>
2024-11-23 14:30:36 -0500

    Built Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import React from "react";
-import { Button } from "@chakra-ui/react";
 
 const App = () => {
   return (
-    <div>
-      <Button colorPalette="red" size="lg">
-        Button
-      </Button>
-      <p>Hi</p>
-    </div>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //>1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </React.StrictMode>
