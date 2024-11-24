c697fb271a71c4ab921edb6ab3ca920a50724df5
William McVicar <wmcvicar@uwaterloo.ca>
2024-11-23 21:03:23 -0500

    Update

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../../assets/logo.webp";
+import logo from "../assets/logo.webp";
 import SearchInput from "./SearchInput";
 import ColorModeSwitch from "./ColorModeSwitch";
 
