81f13417515fde1566057854c37ceac756d9be2b
William McVicar <wmcvicar@uwaterloo.ca>
2024-11-23 20:22:48 -0500

    Added Dynamic Page Heading

---
+++ src/components/GameHeading.tsx
@@ -0,0 +1,21 @@
+import { GameQuery } from "../App";
+import { Heading } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  gameQuery: GameQuery;
+}
+
+const GameHeading = ({ gameQuery }: Props) => {
+  const heading = `${gameQuery.platform?.name || ""} ${
+    gameQuery.genre?.name || ""
+  } Games`;
+
+  return (
+    <Heading as="h1" marginY={5} fontSize="5xl">
+      {heading}
+    </Heading>
+  );
+};
+
+export default GameHeading;
